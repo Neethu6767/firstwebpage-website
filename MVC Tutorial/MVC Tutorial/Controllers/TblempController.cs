cc8d5b2a9311ed080fc4a27a1bcc692e4e54d7a0
Neethu6767 <neethupkollam@gmail.com>
2024-11-07 12:41:22 +0530

    initial comit

---
+++ MVC Tutorial/MVC Tutorial/Controllers/TblempController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MVCTutorial.models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Tutorial.Controllers
+{
+    public class TblempController : Controller
+    {
+        // GET: Tblemp
+        public ActionResult Index1()
+        {
+            List<Employees> employeeList = new List<Employees>();
+=            return View();
+        }
+    }
+}
---
+++ MVC Tutorial/MVC Tutorial/Models/Tblemp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Tutorial.Models
+{
+    public class Tblemp
+    {
+        public int EmployeeID { get; set; }
+        public int Name { get; set; }
+        public int Department { get; set; }
+
+
+    }
+}
