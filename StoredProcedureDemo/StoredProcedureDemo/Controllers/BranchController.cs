d9c8f711503ee1e8d902b930b7d47fdc6fd62377
Neethu6767 <neethupkollam@gmail.com>
2024-11-08 09:46:26 +0530

    initial comit

---
+++ StoredProcedureDemo/StoredProcedureDemo/Controllers/BranchController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StoredProcedureDemo.Models;
+
+namespace StoredProcedureDemo.Controllers
+{
+    
+    public class BranchController : Controller
+    {
+        IMSEntities db = new IMSEntities();
+        // GET: Branch
+        public ActionResult Index()
+        {
+            return View(db.sp_select_branch().ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Branch o)
+        {
+            db.sp_insert_branch(o.BranchId, o.Branchname);
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            var s = db.sp_find_branch(id).FirstOrDefault();
+            return View(s);
+        }
+    }
+}
