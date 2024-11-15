4a443233185a535dfc7dd2b694d5f64da002addd
Neethu6767 <neethupkollam@gmail.com>
2024-11-15 17:35:24 +0530

    initial comit

---
+++ ClientSideValidation/ClientSideValidation/Controllers/DefaultController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using ClientSideValidation.Models;
+
+namespace ClientSideValidation.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Index(Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                return RedirectToAction("Index"); 
+            }
+
+            return View(model);
+        }
+    }
+}
