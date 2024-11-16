2095dd120d63558a4c3bb0a79b980f42444899b9
Neethu6767 <neethupkollam@gmail.com>
2024-11-16 21:43:35 +0530

    initial comit

---
+++ ClientSideValidation/ClientSideValidation/Controllers/ClickController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientSideValidation.Controllers
+{
+    public class ClickController : Controller
+    {
+        // GET: Click
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
